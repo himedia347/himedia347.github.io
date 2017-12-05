ebde470cc7408011c0883b56f4b6034d77d9826e
Olaf Górski <olafgorski@protonmail.com>
2017-12-05 14:33:22 +0100

    add single vid display; hover;

--- js/main.js
+++ js/main.js
@@ -1,29 +1,17 @@
 var movies = JSON.parse(data).movies;
-function addRow(id, title, thumb, movie) {
+function addRow(id, title, thumb) {
     var div = document.createElement('div');
 
     div.className = 'col-sm-3 my-4';
 
     div.innerHTML = '<div class="card">\n' +
-        '    <a onclick="loadVid('+id+');"><img class="card-img-top" src="portfolio/thumbs/'+thumb+'" alt="'+title+'"></a>\n' +
-        '    </div>';
+        '    <a href="single.html?id='+id+'" title="'+title+'"><img class="card-img-top" src="portfolio/thumbs/'+thumb+'" alt="'+title+'"></a>\n' +
+        '    </div>\n';
 
     document.getElementById('portfolio').appendChild(div);
 }
-function loadVid(id){
-    console.log(id);
-    var movie = movies[id-1];
-    $('body').load('https://himedia347.github.io/single.html');
-    var video = document.createElement('div');
-    video.poster=movie.thumb;
-    video.innerHTML = ' <video poster="portfolio/thumbs/'+movie.thumb+'" id="vid" playsinline>\n'+
-    '<source src="portfolio/vid/'+movie.movie+'" type="video/mp4">\n'+
-    '</video>\n';
-    document.getElementById('vid').appendChild(video)
-}
 for(var i = 0; i < movies.length; i++) {
     var obj = movies[i];
-    console.log(obj);
-    addRow(obj.id, obj.title, obj.thumb, obj.movie)
+    addRow(obj.id, obj.title, obj.thumb)
 }
 
---
+++ js/single.js
@@ -0,0 +1,34 @@
+var movies = JSON.parse(data).movies;
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+function loadVid(){
+    var id = getUrlParameter('id');
+    var movie = movies[id-1];
+        var video = document.createElement('div');
+    video.poster=movie.thumb;
+    video.innerHTML = ' <video id="video" poster="portfolio/thumbs/'+movie.thumb+'" id="vid" width="100%"controls playsinline>\n'+
+        '<source src="portfolio/vid/'+movie.movie+'" type="video/mp4">\n'+
+        '</video>\n';
+    document.getElementById('vid').appendChild(video);
+    $("#title").text(movie.title);
+    $("#desc").html(movie.desc);
+}
+
+$( document ).ready(loadVid());
+var video = document.getElementById('video');
+video.addEventListener('click',function(){
+    this.paused?this.play():this.pause();;
+},false);
